4907ee27bceee64a6fb1748019a0debe7d491c07
Ethan <ethanwilson008@gmail.com>
2025-01-31 19:03:44 -0500

    Megatron

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello World!')
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print("""
+                            _                   
+                           | |                  
+ _ __ ___   ___  __ _  __ _| |_ _ __ ___  _ __  
+| '_ ` _ \\ / _ \\/ _` |/ _` | __| '__/ _ \\| '_ \\ 
+| | | | | |  __/ (_| | (_| | |_| | | (_) | | | |
+|_| |_| |_|\\___|\\__, |\\__,_|\\__|_|  \\___/|_| |_|
+                 __/ |                          
+                |___/                           
+    """)
+    
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('')
